bb258d70695fa89e590f6a20e9510b278a02c073
jacobharrison <jacob.harrison@qa.com>
2018-08-03 14:43:35 +0100

    Added additonal step where user is created in user story

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/steps/JobServiceStep.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/steps/JobServiceStep.java
@@ -2,6 +2,7 @@ package uk.gov.ons.fwmt.acceptancetests.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.io.FileSystemResource;
@@ -16,24 +17,42 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.ons.fwmt.acceptancetests.dto.JobDto;
+import uk.gov.ons.fwmt.acceptancetests.dto.UserDto;
 import uk.gov.ons.fwmt.acceptancetests.helper.ResourceRESTHelper;
 
 import java.io.File;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Component
 @Slf4j
 public class JobServiceStep {
 
+  private String userName = "user";
+  private String password = "password";
+
   private RestTemplate resourcesRestTemplate(RestTemplateBuilder builder) {
     return builder
-        .basicAuthorization("user", "password")
+        .basicAuthorization(userName, password)
         .build();
   }
 
-  @Given("^I have submitted a sample CSV of type LFS named \"([^\"]*)\"$")
+  @Given("^I have created a user in the gateway$")
+  public void iHaveCreatedAUserInTheGateway() {
+    String authNo = "INT0";
+    String tmUsername = "string";
+
+    UserDto userDto = UserDto.builder().authNo(authNo).tmUsername(tmUsername).active(true).alternateAuthNo(null)
+        .build();
+
+    final RestTemplate restTemplate = resourcesRestTemplate(new RestTemplateBuilder());
+    String location = "http://localhost:9093/resources/users";
+    final Optional<UserDto> resultUserDto = ResourceRESTHelper.post(restTemplate, location, userDto, UserDto.class);
+  }
+
+  @When("^I submit a sample CSV of type LFS named \"([^\"]*)\"$")
   public void iHaveASampleCSVOfType(String fileName) {
     int expectedHttpStatusCode = 200;
     String location = "http://localhost:9091/jobs/samples";
@@ -56,10 +75,11 @@ public class JobServiceStep {
   @Then("^A database record for the job should contain TM id \"([^\"]*)\"$")
   public void aDatabaseRecordForTheJobShouldContainTMId(String tmJobId) {
     final RestTemplate restTemplate = resourcesRestTemplate(new RestTemplateBuilder());
-    String location = "http://localhost:9093/resources/" + tmJobId;
+    String location = "http://localhost:9093/resources/jobs" + tmJobId;
 
     final Optional<JobDto> jobDto = ResourceRESTHelper.get(restTemplate, location, JobDto.class, tmJobId);
 
-    assertEquals(tmJobId, jobDto.get().getTmJobId());
+    assertTrue(jobDto.isPresent());
+    assertEquals(tmJobId, jobDto.toString());
   }
 }
