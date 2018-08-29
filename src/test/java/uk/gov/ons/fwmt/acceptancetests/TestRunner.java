192cbd683b71ab8e5a6315ead5ce4864758395fa
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2018-08-29 17:06:41 +0100

    Added json report

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/TestRunner.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
+@CucumberOptions(plugin = {"pretty","json:build/cucumber-report.json"},
   features = {"src/test/resources/acceptancetests/test.feature"},
   glue = {"uk.gov.ons.fwmt.acceptancetests"})
 public class TestRunner {
