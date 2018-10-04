05614b46e3254077c425619224e1c905f82b7506
Chris Hardman <christopher.hardman@ons.gov.uk>
2018-10-04 09:52:54 +0100

    FWMT-567: add smoke test for legacy services

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/smoketest/GatewaySmokeTestSteps.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/smoketest/GatewaySmokeTestSteps.java
@@ -37,7 +37,7 @@ public class GatewaySmokeTestSteps {
         Assert.assertTrue(result.contains("\"status\":\"UP\""));
     }
 
-    @Given("^Check Jobservice is running$")
+    @Given("^Check Job Service is running$")
     public void checkJobserviceRunning() throws Exception {
 
         final String plainCreds = "user:password";
@@ -54,6 +54,75 @@ public class GatewaySmokeTestSteps {
         HttpEntity<String> request = new HttpEntity<String>(headers);
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
         String result = response.getBody();
+
+        Assert.assertTrue(result.contains("\"status\":\"UP\""));
+    }
+
+    @Given("^Check Resource Service is running$")
+    public void checkResourceServiceRunning() throws Exception {
+
+        final String plainCreds = "user:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        final String url = "http://localhost:9093/resources/health";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        String result = response.getBody();
+
+        System.out.println(result);
+
+        Assert.assertTrue(result.contains("\"status\":\"UP\""));
+    }
+
+    @Given("^Check Legacy Jobservice is running$")
+    public void checkLegacyJobserviceRunning() throws Exception {
+
+        final String plainCreds = "user:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        final String url = "http://localhost:9091/jobs/health";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        String result = response.getBody();
+
+        System.out.println(result);
+
+        Assert.assertTrue(result.contains("\"status\":\"UP\""));
+    }
+
+    @Given("^Check Staff Service is running$")
+    public void checkStaffserviceRunning() throws Exception {
+
+        final String plainCreds = "user:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        final String url = "http://localhost:9092/staff/health";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        String result = response.getBody();
+
+        System.out.println(result);
 
         Assert.assertTrue(result.contains("\"status\":\"UP\""));
     }
--- src/test/resources/smoketest/gateway.feature
+++ src/test/resources/smoketest/gateway.feature
@@ -1,10 +1,18 @@
 Feature: Smoke test for the FWMT gateway version 2
 
-  @smoke
- Scenario: smoke test
-    When Check Tmoblie is running
-#    When Check Rabbit is running
-    #When Check Jobservice is running
-    #When Check RmAdpater is running
-    #
+ #@smoke1
+ #Scenario: smoke test gateway
+ #   When Check Tmoblie is running
+ #   When Check Rabbit is running
+ #   When Check Job Service is running
+ #   When Check RmAdpater is running
+
+ @smoke2
+ Scenario: smoke test legacy gateway
+   When Check Staff Service is running
+   When Check Resource Service is running
+   When Check Tmoblie is running
+   When Check Legacy Jobservice is running
+
+
 
