023e220faedea2172df683fc5ff11d0ee0877a39
jacobharrison <jacob.harrison@qa.com>
2018-11-01 13:31:09 +0000

    SEPARATION OF CENSUS ACCEPTANCE TESTS

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/utils/Config.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/utils/Config.java
@@ -10,8 +10,6 @@ public class Config {
 
   public static final String BASIC_AUTH_PASSWORD = System.getenv("BASIC_AUTH_PASSWORD");
 
-  public static final String MOCK_URL = System.getenv("MOCK_URL");
-
   //Smoke Tests
   public static final String RMA_URL = System.getenv("RMA_URL");
 
@@ -30,19 +28,4 @@ public class Config {
   public static final String TM_PASSWORD = System.getenv("TM_PASSWORD");
 
   public static final String TM_URL = System.getenv("TM_URL");
-
-
-  //Legacy Smoke Tests
-  public static final String RS_USERNAME = System.getenv("RS_USERNAME");
-  public static final String RS_PASSWORD = System.getenv("RS_PASSWORD");
-  public static final String RS_URL = System.getenv("RS_URL");
-
-  public static final String LS_USERNAME = System.getenv("LS_USERNAME");
-  public static final String LS_PASSWORD = System.getenv("LS_PASSWORD");
-  public static final String LS_URL = System.getenv("LS_URL");
-  
-  public static final String SS_USERNAME = System.getenv("SS_USERNAME");
-  public static final String SS_PASSWORD = System.getenv("SS_PASSWORD");
-  public static final String SS_URL = System.getenv("SS_URL");
-
 }
