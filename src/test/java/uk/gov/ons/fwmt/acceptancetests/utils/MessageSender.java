0c9228d7beb93156713bdf6720572a4cad3a77a7
Simon <sdiaz1000@gmail.com>
2018-11-06 15:13:21 +0000

    fwmt-599 get messages from mock

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/utils/MessageSender.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/utils/MessageSender.java
@@ -1,13 +1,15 @@
 package uk.gov.ons.fwmt.acceptancetests.utils;
 
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Consumer;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
-import lombok.NoArgsConstructor;
+import static uk.gov.ons.fwmt.acceptancetests.utils.Config.BASIC_AUTH_PASSWORD;
+import static uk.gov.ons.fwmt.acceptancetests.utils.Config.BASIC_AUTH_USER;
+import static uk.gov.ons.fwmt.acceptancetests.utils.Config.JOB_SVC_URL;
+import static uk.gov.ons.fwmt.acceptancetests.utils.Config.TM_RESPONSE_ENDPOINT;
+import static uk.gov.ons.fwmt.acceptancetests.utils.Config.MOCK_URL;
+import static uk.gov.ons.fwmt.fwmtgatewaycommon.config.QueueNames.ADAPTER_TO_RM_QUEUE;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -16,42 +18,16 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import lombok.extern.slf4j.Slf4j;
 
-import static uk.gov.ons.fwmt.acceptancetests.utils.Config.BASIC_AUTH_PASSWORD;
-import static uk.gov.ons.fwmt.acceptancetests.utils.Config.BASIC_AUTH_USER;
-import static uk.gov.ons.fwmt.acceptancetests.utils.Config.JOB_SVC_URL;
-import static uk.gov.ons.fwmt.acceptancetests.utils.Config.TM_RESPONSE_ENDPOINT;
-import static uk.gov.ons.fwmt.fwmtgatewaycommon.config.QueueNames.ADAPTER_TO_RM_QUEUE;
-
-
+@Slf4j
 public class MessageSender {
 
-  private final String USER_AGENT = "Mozilla/5.0";
-
-  private static String message;
-  private static int messageCount;
 
   public MessageSender() throws IOException, TimeoutException {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
-
-    channel.queueDeclarePassive(ADAPTER_TO_RM_QUEUE);
-    Consumer consumer = new DefaultConsumer(channel) {
-      @Override
-      public void handleDelivery(String consumerTag, Envelope envelope,
-          AMQP.BasicProperties properties, byte[] body)
-          throws IOException {
-        message = new String(body, "UTF-8");
-        messageCount++;
-      }
-    };
-
-    channel.basicConsume(ADAPTER_TO_RM_QUEUE, true, consumer);
   }
 
   public int sendResponseMessage(String data) throws IOException, AuthenticationException {
@@ -77,13 +53,21 @@ public class MessageSender {
     return resopnseCode;
   }
 
-  public int getMessageCount() {
-    return messageCount;
+  public long getMessageCount() {
+    RestTemplate restTemplate = new RestTemplate();
+    String messageCountUrl = MOCK_URL + "/queue/count/" + ADAPTER_TO_RM_QUEUE;
+    ResponseEntity<Long> messageCount = restTemplate.getForEntity(messageCountUrl, Long.class);
+    return messageCount.getBody();
   }
 
   public String getMessage() throws InterruptedException {
-
+    RestTemplate restTemplate = new RestTemplate();
+    String messageUrl = MOCK_URL + "/queue/message/" + ADAPTER_TO_RM_QUEUE;
+    String message = null;
     for(int i=0;i<10;i++) {
+      ResponseEntity<String> messageEntity = restTemplate.getForEntity(messageUrl, String.class);
+      message = messageEntity.getBody();
+      
       if(message != null) {
         break;
       }
