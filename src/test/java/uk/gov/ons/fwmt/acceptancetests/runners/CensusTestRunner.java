83c6cb9f8b15a281ead668b605a9192940e012a3
garethTurner <gareth.turner@ons.gov.uk>
2018-12-04 15:00:30 +0000

    use different names for json test reports

--- src/test/java/uk/gov/ons/fwmt/acceptancetests/runners/CensusTestRunner.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/runners/CensusTestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","json:build/cucumber-report.json"},
+@CucumberOptions(plugin = {"pretty","json:build/census-report.json"},
     features = {"src/test/resources/acceptancetests/CensusTests.feature"},
     glue = {"uk.gov.ons.fwmt.acceptancetests.steps.census"})
 public class CensusTestRunner {
--- src/test/java/uk/gov/ons/fwmt/acceptancetests/runners/OHSTestRunner.java
+++ src/test/java/uk/gov/ons/fwmt/acceptancetests/runners/OHSTestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","json:build/cucumber-report.json"},
+@CucumberOptions(plugin = {"pretty","json:build/OHS-report.json"},
     features = {"src/test/resources/acceptancetests/OHSTests.feature"},
     glue = {"uk.gov.ons.fwmt.acceptancetests.steps/OHS"})
 public class OHSTestRunner {
